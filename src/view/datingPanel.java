b8b4d804f5ccde4fcee9696bc8647183ff5d825a
squidlywizard <ztdenna@hotmail.com>
2019-03-21 09:05:26 -0600

    put in the image

--- src/view/datingPanel.java
+++ src/view/datingPanel.java
@@ -2,6 +2,7 @@ package view;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import view.image.*;
 
 
 import javax.swing.*;
@@ -10,13 +11,19 @@ import controller.datingController;
 
 public class datingPanel extends JPanel 
 {
+
 private JButton yes;
 private JButton no;
+private JButton next;
 private JTextField dates;
 private ImageIcon demi;
 	public datingPanel(datingController appController) {
 		super();
-	
+	yes = new JButton ("y-yes");
+	no = new JButton("n-no");
+	next = new JButton("next");
+	dates = new JTextField("im odea, im looking for love", 600);
+	demi = new ImageIcon(getClass().getResource("/view/image/demiFinal.png"));
 	
 
 	
@@ -26,6 +33,11 @@ private ImageIcon demi;
 
  public void setupPanel()
  {
+	this.add(yes);
+	this.add(no);
+	this.add(next);
+	this.add(dates);
+	
 	
  }
  public void setupLayout()
